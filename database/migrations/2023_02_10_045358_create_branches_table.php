5f91dd2913c1f9c7a6b96cd751c57e6f7dbf94c0
Raxit Pansuriya <raxit.pansuriya@kanishkasoftware.com>
2023-02-10 11:11:55 +0530

    add branch migration

---
+++ database/migrations/2023_02_10_045358_create_branches_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('branch_code');
+            $table->string('branch_manager');
+            $table->string('contact_number');
+            $table->integer('is_active')->default('1')->comment('1=active, 0=deactive');
+            $table->integer('branch_serving')->nullable()->comment('1st 0=breakfast, 2nd 0=lunch, 3rd 0=snacks, 4th 0=dinner');
+            $table->integer('serving_type')->nullable()->comment('1st 0=veg, 2nd 0=nonveg, 3rd 0=bevarage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+}
