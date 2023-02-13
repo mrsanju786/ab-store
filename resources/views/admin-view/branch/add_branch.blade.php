aa92445ab7c4bb850915ae40f0aae656ca004c2d
unknown <dipak.patil@kanishkasoftware.com>
2023-02-13 11:19:15 +0530

    UI form design

--- resources/views/admin-view/branch/add_branch.blade.php
+++ resources/views/admin-view/branch/add_branch.blade.php
@@ -19,171 +19,184 @@
             </div>
             <!--end::Card header-->
 
-            <!--begin::Card body-->
-            <div class="card-body pt-1">
-                <!--begin::Form-->
-                <form action="{{route('create-branch')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <!--begin::Scroll-->
-                    <div class="d-flex flex-column scroll-y me-n7 pe-7" id="kt_modal_add_role_scroll">
+           
+            <!--begin::Row-->
+            <div class="row row-cols-1 row-cols-sm-2 rol-cols-md-1 row-cols-lg-2">
 
-                        <!--begin::Input group-->
-                        <div class="fv-row mb-10 fv-plugins-icon-container">
-                            <!--begin::Label-->
-                            <label class="fs-5 fw-bold form-label mb-2">
-                                <span class="required">Select Region</span>
-                            </label>
-                            <!--end::Label-->
+                <!--begin::Col-->
+                <div class="col">
 
-                            <!--begin::Input-->
-                            <select class="form-select" aria-label="Select example" name="region_id">
-                                <option>Select Region</option>
-                                @foreach($region_list as $region)
-                                <option value="{{$region->id}}">{{$region->region_name}}</option>
-                                @endforeach
-                                
-                            </select>
-                            <!--end::Input-->
-                            @if($errors->has('region_id'))
-                            <span class="text-danger">{{ $errors->first('region_id') }}</span>
-                            @endif
-                            <div class="fv-plugins-message-container invalid-feedback"></div>
-                        </div>
-                        <!--end::Input group-->
+                    <!--begin::Card body-->
+                    <div class="card-body pt-1">
 
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-10 fv-plugins-icon-container">
-                                <!--begin::Label-->
-                                <label class="fs-5 fw-bold form-label mb-2">
-                                    <span class="required">Branch Name</span>
-                                </label>
-                                <!--end::Label-->
+                        <!--begin::Form-->
+                        <form action="{{route('create-branch')}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <!--begin::Scroll-->
+                            <div class="d-flex flex-column scroll-y me-n7 pe-7" id="kt_modal_add_role_scroll">
 
-                                <!--begin::Input-->
-                                <input class="form-control form-control-solid" placeholder="Enter Branch Name"
-                                    name="branch_name">
-                                <!--end::Input-->
-                                @if($errors->has('branch_name'))
-                                <span class="text-danger">{{ $errors->first('branch_name') }}</span>
-                                @endif
-                                <div class="fv-plugins-message-container invalid-feedback"></div>
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Select Region</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <select class="form-select" aria-label="Select example" name="region_id">
+                                        <option>Select Region</option>
+                                        @foreach($region_list as $region)
+                                        <option value="{{$region->id}}">{{$region->region_name}}</option>
+                                        @endforeach
+                                        
+                                    </select>
+                                    <!--end::Input-->
+                                    @if($errors->has('region_id'))
+                                    <span class="text-danger">{{ $errors->first('region_id') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Branch Name</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Enter Branch Name"
+                                        name="branch_name">
+                                    <!--end::Input-->
+                                    @if($errors->has('branch_name'))
+                                    <span class="text-danger">{{ $errors->first('branch_name') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Branch Code</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Enter Branch Code"
+                                        name="branch_code">
+                                    <!--end::Input-->
+                                    @if($errors->has('branch_code'))
+                                    <span class="text-danger">{{ $errors->first('branch_code') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Branch Manager</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Branch Manager"
+                                        name="branch_manager">
+                                    <!--end::Input-->
+                                    @if($errors->has('branch_manager'))
+                                    <span class="text-danger">{{ $errors->first('branch_manager') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Contact Number</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Contact Number"
+                                        name="contact_number">
+                                    <!--end::Input-->
+                                    @if($errors->has('contact_number'))
+                                    <span class="text-danger">{{ $errors->first('contact_number') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Branch Serving</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Branch Serving"
+                                        name="branch_serving">
+                                    <!--end::Input-->
+                                    @if($errors->has('branch_serving'))
+                                    <span class="text-danger">{{ $errors->first('branch_serving') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Serving Type</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Serving Type"
+                                        name="serving_type">
+                                    <!--end::Input-->
+                                    @if($errors->has('serving_type'))
+                                    <span class="text-danger">{{ $errors->first('serving_type') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Actions-->
+                                <div class="text-left pt-3">
+
+                                    <button type="submit" class="btn btn-lg btn-primary"
+                                        data-kt-roles-modal-action="submit">
+                                        <span class="indicator-label">
+                                            Submit
+                                        </span>
+                                        <span class="indicator-progress">
+                                            Please wait... <span
+                                                class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                        </span>
+                                    </button>
+                                </div>
+                                <!--end::Actions-->
+
                             </div>
-                            <!--end::Input group-->
+                        </form>
+                        <!--end::Form-->
+                        
+                    </div>
+                    <!--end::Card body-->
 
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-10 fv-plugins-icon-container">
-                                <!--begin::Label-->
-                                <label class="fs-5 fw-bold form-label mb-2">
-                                    <span class="required">Branch Code</span>
-                                </label>
-                                <!--end::Label-->
 
-                                <!--begin::Input-->
-                                <input class="form-control form-control-solid" placeholder="Enter Branch Code"
-                                    name="branch_code">
-                                <!--end::Input-->
-                                @if($errors->has('branch_code'))
-                                <span class="text-danger">{{ $errors->first('branch_code') }}</span>
-                                @endif
-                                <div class="fv-plugins-message-container invalid-feedback"></div>
-                            </div>
-                            <!--end::Input group-->
-
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-10 fv-plugins-icon-container">
-                                <!--begin::Label-->
-                                <label class="fs-5 fw-bold form-label mb-2">
-                                    <span class="required">Branch Manager</span>
-                                </label>
-                                <!--end::Label-->
-
-                                <!--begin::Input-->
-                                <input class="form-control form-control-solid" placeholder="Branch Manager"
-                                    name="branch_manager">
-                                <!--end::Input-->
-                                @if($errors->has('branch_manager'))
-                                <span class="text-danger">{{ $errors->first('branch_manager') }}</span>
-                                @endif
-                                <div class="fv-plugins-message-container invalid-feedback"></div>
-                            </div>
-                            <!--end::Input group-->
-
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-10 fv-plugins-icon-container">
-                                <!--begin::Label-->
-                                <label class="fs-5 fw-bold form-label mb-2">
-                                    <span class="required">Contact Number</span>
-                                </label>
-                                <!--end::Label-->
-
-                                <!--begin::Input-->
-                                <input class="form-control form-control-solid" placeholder="Contact Number"
-                                    name="contact_number">
-                                <!--end::Input-->
-                                @if($errors->has('contact_number'))
-                                <span class="text-danger">{{ $errors->first('contact_number') }}</span>
-                                @endif
-                                <div class="fv-plugins-message-container invalid-feedback"></div>
-                            </div>
-                            <!--end::Input group-->
-
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-10 fv-plugins-icon-container">
-                                <!--begin::Label-->
-                                <label class="fs-5 fw-bold form-label mb-2">
-                                    <span class="required">Branch Serving</span>
-                                </label>
-                                <!--end::Label-->
-
-                                <!--begin::Input-->
-                                <input class="form-control form-control-solid" placeholder="Branch Serving"
-                                    name="branch_serving">
-                                <!--end::Input-->
-                                @if($errors->has('branch_serving'))
-                                <span class="text-danger">{{ $errors->first('branch_serving') }}</span>
-                                @endif
-                                <div class="fv-plugins-message-container invalid-feedback"></div>
-                            </div>
-                            <!--end::Input group-->
-
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-10 fv-plugins-icon-container">
-                                <!--begin::Label-->
-                                <label class="fs-5 fw-bold form-label mb-2">
-                                    <span class="required">Serving Type</span>
-                                </label>
-                                <!--end::Label-->
-
-                                <!--begin::Input-->
-                                <input class="form-control form-control-solid" placeholder="Serving Type"
-                                    name="serving_type">
-                                <!--end::Input-->
-                                @if($errors->has('serving_type'))
-                                <span class="text-danger">{{ $errors->first('serving_type') }}</span>
-                                @endif
-                                <div class="fv-plugins-message-container invalid-feedback"></div>
-                            </div>
-                            <!--end::Input group-->
-
-                            <!--begin::Actions-->
-                            <div class="text-left pt-3">
-
-                                <button type="submit" class="btn btn-lg btn-primary"
-                                    data-kt-roles-modal-action="submit">
-                                    <span class="indicator-label">
-                                        Submit
-                                    </span>
-                                    <span class="indicator-progress">
-                                        Please wait... <span
-                                            class="spinner-border spinner-border-sm align-middle ms-2"></span>
-                                    </span>
-                                </button>
-                            </div>
-                            <!--end::Actions-->
-                    </form>
-                    <!--end::Form-->
-                </div>
-                <!--end::Card body-->
                 </div>
                 <!--end::Col-->
 
