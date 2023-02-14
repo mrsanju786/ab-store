11d0417ac5a918b66cd0ac92a9ffeff369faefdb
sachin tripathi <sachin.tripathi>
2023-02-14 12:39:09 +0530

    sidebar menus added

--- resources/views/admin-view/layouts/partials/sidebar.blade.php
+++ resources/views/admin-view/layouts/partials/sidebar.blade.php
@@ -111,30 +111,7 @@
                     <!--end:Menu link-->
                 </div>
 
-                <div class="menu-item">
-                    <!--begin:Menu link-->
-                    <a class="menu-link {{ request()->is('admin/company-list') ? 'active' : '' }}" href="{{route('company_list')}}">
-                        <span class="menu-icon">
-                            <!--begin::Svg Icon | path: icons/duotune/general/gen014.svg-->
-                            <span class="svg-icon svg-icon-2"><svg width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect x="2" y="2" width="9" height="9" rx="2" fill="currentColor"></rect>
-                                    <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="currentColor">
-                                    </rect>
-                                    <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="currentColor">
-                                    </rect>
-                                    <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="currentColor">
-                                    </rect>
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
-                        </span>
-                        <span class="menu-title">Company</span>
-                    </a>
-                    <!--end:Menu link-->
-                </div>
-
-                <div class="menu-item">
+                {{--<div class="menu-item">
                     <!--begin:Menu link-->
                     <a class="menu-link {{ request()->is('admin/foodlicense-list') ? 'active' : '' }}" href="{{route('foodlicense_list')}}">
                         <span class="menu-icon">
@@ -155,31 +132,62 @@
                         <span class="menu-title">Food License</span>
                     </a>
                     <!--end:Menu link-->
-                </div>
+                </div> --}}
 
-                <div class="menu-item">
-                    <!--begin:Menu link-->
-                    <a class="menu-link {{ request()->is('admin/region-list') ? 'active' : '' }}" href="{{route('region_list')}}">
-                        <span class="menu-icon">
-                            <!--begin::Svg Icon | path: icons/duotune/general/gen014.svg-->
-                            <span class="svg-icon svg-icon-2"><svg width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect x="2" y="2" width="9" height="9" rx="2" fill="currentColor"></rect>
-                                    <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="currentColor">
-                                    </rect>
-                                    <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="currentColor">
-                                    </rect>
-                                    <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="currentColor">
-                                    </rect>
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion  {{ request()->is('admin/master/*') ? 'show' : '' }}">
+                <!--begin:Menu link-->
+                <span class="menu-link">
+                    <span class="menu-icon">
+                        <!--begin::Svg Icon | path: icons/duotune/communication/com005.svg-->
+                        <span class="svg-icon svg-icon-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20 14H18V10H20C20.6 10 21 10.4 21 11V13C21 13.6 20.6 14 20 14ZM21 19V17C21 16.4 20.6 16 20 16H18V20H20C20.6 20 21 19.6 21 19ZM21 7V5C21 4.4 20.6 4 20 4H18V8H20C20.6 8 21 7.6 21 7Z" fill="currentColor"></path>
+                            <path opacity="0.3" d="M17 22H3C2.4 22 2 21.6 2 21V3C2 2.4 2.4 2 3 2H17C17.6 2 18 2.4 18 3V21C18 21.6 17.6 22 17 22ZM10 7C8.9 7 8 7.9 8 9C8 10.1 8.9 11 10 11C11.1 11 12 10.1 12 9C12 7.9 11.1 7 10 7ZM13.3 16C14 16 14.5 15.3 14.3 14.7C13.7 13.2 12 12 10.1 12C8.10001 12 6.49999 13.1 5.89999 14.7C5.59999 15.3 6.19999 16 7.39999 16H13.3Z" fill="currentColor"></path>
+                            </svg>
                         </span>
-                        <span class="menu-title">Region</span>
-                    </a>
-                    <!--end:Menu link-->
+                        <!--end::Svg Icon-->
+                    </span>
+                    <span class="menu-title">Master Setting</span><span class="menu-arrow"></span>
+                </span>
+                <!--end:Menu link--><!--begin:Menu sub-->
+                <div class="menu-sub menu-sub-accordion {{ request()->is('admin/master/*') ? 'show' : '' }}" kt-hidden-height="242" style=" ">
+                    <!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('company_list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Company</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('country_list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Country</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('state_list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">State</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('city_list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">City</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('region_list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Region</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('license-list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">License</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('currency-list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Currency</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item-->
+                    <!--end:Menu item--><!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link--><a class="menu-link" href="{{route('timezone-list')}}"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Timezone</span></a><!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item-->
                 </div>
-
+                <!--end:Menu sub-->
+                </div>
                 <div class="menu-item">
                     <!--begin:Menu link-->
                     <a class="menu-link {{ request()->is('admin/branch-list') ? 'active' : '' }}" href="{{route('branch_list')}}">
--- routes/web.php
+++ routes/web.php
@@ -77,65 +77,97 @@ Route::group(['prefix' => 'admin'], function()
         Route::post('/create-user', [UserController::class, 'createUser'])->name('create-user');
         Route::get('/edit-user/{id}', [UserController::class, 'editUser'])->name('edit-user');
         Route::post('/update-user/{id}', [UserController::class, 'updateUser'])->name('update-user');
+        
+        Route::group(['prefix' => 'master'], function()
+        {
+            /**
+             * License Routes
+             */
+            Route::get('/license-list', [LicenseController::class, 'index'])->name('license-list');
+            Route::get('/add-license', [LicenseController::class, 'addLicense'])->name('add-license');
+            Route::post('/create-license', [LicenseController::class, 'createLicense'])->name('create-license');
+            Route::get('/edit-license/{id}', [LicenseController::class, 'editLicense'])->name('edit-license');
+            Route::post('/update-license', [LicenseController::class, 'updateLicense'])->name('update-license');
 
-        /**
-         * License Routes
-         */
-        Route::get('/license-list', [LicenseController::class, 'index'])->name('license-list');
-        Route::get('/add-license', [LicenseController::class, 'addLicense'])->name('add-license');
-        Route::post('/create-license', [LicenseController::class, 'createLicense'])->name('create-license');
-        Route::get('/edit-license/{id}', [LicenseController::class, 'editLicense'])->name('edit-license');
-        Route::post('/update-license', [LicenseController::class, 'updateLicense'])->name('update-license');
+            /**
+             * Currency Routes
+             */
+            Route::get('/currency-list', [CurrencyController::class, 'index'])->name('currency-list');
+            Route::get('/add-currency', [CurrencyController::class, 'addCurrency'])->name('add-currency');
+            Route::post('/create-currency', [CurrencyController::class, 'createCurrency'])->name('create-currency');
+            Route::get('/edit-currency/{id}', [CurrencyController::class, 'editCurrency'])->name('edit-currency');
+            Route::post('/update-currency', [CurrencyController::class, 'updateCurrency'])->name('update-currency');
 
-        /**
-         * Currency Routes
-         */
-        Route::get('/currency-list', [CurrencyController::class, 'index'])->name('currency-list');
-        Route::get('/add-currency', [CurrencyController::class, 'addCurrency'])->name('add-currency');
-        Route::post('/create-currency', [CurrencyController::class, 'createCurrency'])->name('create-currency');
-        Route::get('/edit-currency/{id}', [CurrencyController::class, 'editCurrency'])->name('edit-currency');
-        Route::post('/update-currency', [CurrencyController::class, 'updateCurrency'])->name('update-currency');
-
-        /**
-         * Timezone Routes
-         */
-        Route::get('/timezone-list', [TimezoneController::class, 'index'])->name('timezone-list');
-        Route::get('/add-timezone', [TimezoneController::class, 'addTimezone'])->name('add-timezone');
-        Route::post('/create-timezone', [TimezoneController::class, 'createTimezone'])->name('create-timezone');
-        Route::get('/edit-timezone/{id}', [TimezoneController::class, 'editTimezone'])->name('edit-timezone');
-        Route::post('/update-timezone', [TimezoneController::class, 'updateTimezone'])->name('update-timezone');
-
-
-        /**
-         * Company Routes
-         */
-        Route::get('/company-list', [CompanyController::class, 'index'])->name('company_list');
-        Route::get('/add-company', [CompanyController::class, 'addCompany'])->name('add-company');
-        Route::post('/create-company', [CompanyController::class, 'createCompany'])->name('create-company');
-        Route::get('/edit-company/{id}', [CompanyController::class, 'editCompany'])->name('edit-company');
-        Route::post('/update-company', [CompanyController::class, 'updateCompany'])->name('update-company');
-        Route::get('/company-status/{id}/{status}', [CompanyController::class, 'companyStatus'])->name('company-status');
-
-        /**
-         * Foodlicense Routes
-         */
-        Route::get('/foodlicense-list', [FoodlicenseController::class, 'index'])->name('foodlicense_list');
-        Route::get('/add-foodlicense', [FoodlicenseController::class, 'addFoodlicense'])->name('add-foodlicense');
-        Route::post('/create-foodlicense', [FoodlicenseController::class, 'createFoodlicense'])->name('create-foodlicense');
-        Route::get('/edit-foodlicense/{id}', [FoodlicenseController::class, 'editFoodlicense'])->name('edit-foodlicense');
-        Route::post('/update-foodlicense', [FoodlicenseController::class, 'updateFoodlicense'])->name('update-foodlicense');
+            /**
+             * Timezone Routes
+             */
+            Route::get('/timezone-list', [TimezoneController::class, 'index'])->name('timezone-list');
+            Route::get('/add-timezone', [TimezoneController::class, 'addTimezone'])->name('add-timezone');
+            Route::post('/create-timezone', [TimezoneController::class, 'createTimezone'])->name('create-timezone');
+            Route::get('/edit-timezone/{id}', [TimezoneController::class, 'editTimezone'])->name('edit-timezone');
+            Route::post('/update-timezone', [TimezoneController::class, 'updateTimezone'])->name('update-timezone');
 
 
-        /**
-         * Region Routes
-         */
-        Route::get('/region-list', [RegionController::class, 'index'])->name('region_list');
-        Route::get('/add-region', [RegionController::class, 'addRegion'])->name('add-region');
-        Route::post('/create-region', [RegionController::class, 'createRegion'])->name('create-region');
-        Route::get('/edit-region/{id}', [RegionController::class, 'editRegion'])->name('edit-region');
-        Route::post('/update-region', [RegionController::class, 'updateRegion'])->name('update-region');
-        Route::get('/region-status/{id}/{status}', [RegionController::class, 'regionStatus'])->name('region-status');
+            /**
+             * Company Routes
+             */
+            Route::get('/company-list', [CompanyController::class, 'index'])->name('company_list');
+            Route::get('/add-company', [CompanyController::class, 'addCompany'])->name('add-company');
+            Route::post('/create-company', [CompanyController::class, 'createCompany'])->name('create-company');
+            Route::get('/edit-company/{id}', [CompanyController::class, 'editCompany'])->name('edit-company');
+            Route::post('/update-company', [CompanyController::class, 'updateCompany'])->name('update-company');
+            Route::get('/company-status/{id}/{status}', [CompanyController::class, 'companyStatus'])->name('company-status');
 
+            /**
+             * Foodlicense Routes
+             */
+            Route::get('/foodlicense-list', [FoodlicenseController::class, 'index'])->name('foodlicense_list');
+            Route::get('/add-foodlicense', [FoodlicenseController::class, 'addFoodlicense'])->name('add-foodlicense');
+            Route::post('/create-foodlicense', [FoodlicenseController::class, 'createFoodlicense'])->name('create-foodlicense');
+            Route::get('/edit-foodlicense/{id}', [FoodlicenseController::class, 'editFoodlicense'])->name('edit-foodlicense');
+            Route::post('/update-foodlicense', [FoodlicenseController::class, 'updateFoodlicense'])->name('update-foodlicense');
+
+
+            /**
+             * Region Routes
+             */
+            Route::get('/region-list', [RegionController::class, 'index'])->name('region_list');
+            Route::get('/add-region', [RegionController::class, 'addRegion'])->name('add-region');
+            Route::post('/create-region', [RegionController::class, 'createRegion'])->name('create-region');
+            Route::get('/edit-region/{id}', [RegionController::class, 'editRegion'])->name('edit-region');
+            Route::post('/update-region', [RegionController::class, 'updateRegion'])->name('update-region');
+            Route::get('/region-status/{id}/{status}', [RegionController::class, 'regionStatus'])->name('region-status');
+
+
+            /**
+             * Country Routes
+             */
+            Route::get('/country-list', [CountryController::class, 'index'])->name('country_list');
+            Route::get('/add-country', [CountryController::class, 'addCountry'])->name('add-country');
+            Route::post('/create-country', [CountryController::class, 'createCountry'])->name('create-country');
+            Route::get('/edit-country/{id}', [CountryController::class, 'editCountry'])->name('edit-country');
+            Route::post('/update-country', [CountryController::class, 'updateCountry'])->name('update-country');
+
+            /**
+             * State Routes
+             */
+            Route::get('/state-list', [StateController::class, 'index'])->name('state_list');
+            Route::get('/add-state', [StateController::class, 'addState'])->name('add-state');
+            Route::post('/create-state', [StateController::class, 'createState'])->name('create-state');
+            Route::get('/edit-state/{id}', [StateController::class, 'editState'])->name('edit-state');
+            Route::post('/update-state', [StateController::class, 'updateState'])->name('update-state');
+            Route::get('/get-state/{id}', [StateController::class, 'getState'])->name('get-state');
+
+            /**
+             * City Routes
+             */
+            Route::get('/city-list', [CityController::class, 'index'])->name('city_list');
+            Route::get('/add-city', [CityController::class, 'addCity'])->name('add-city');
+            Route::post('/create-city', [CityController::class, 'createCity'])->name('create-city');
+            Route::get('/edit-city/{id}', [CityController::class, 'editCity'])->name('edit-city');
+            Route::post('/update-city', [CityController::class, 'updateCity'])->name('update-city');
+            Route::get('/get-city/{id}', [CityController::class, 'getCity'])->name('get-city');
+        });
 
         /**
          * Branch Routes
@@ -146,36 +178,6 @@ Route::group(['prefix' => 'admin'], function()
         Route::get('/edit-branch/{id}', [BranchController::class, 'editBranch'])->name('edit-branch');
         Route::post('/update-branch', [BranchController::class, 'updateBranch'])->name('update-branch');
         Route::get('/branch-status/{id}/{status}', [BranchController::class, 'branchStatus'])->name('branch-status');
-
-
-        /**
-         * Country Routes
-         */
-        Route::get('/country-list', [CountryController::class, 'index'])->name('country_list');
-        Route::get('/add-country', [CountryController::class, 'addCountry'])->name('add-country');
-        Route::post('/create-country', [CountryController::class, 'createCountry'])->name('create-country');
-        Route::get('/edit-country/{id}', [CountryController::class, 'editCountry'])->name('edit-country');
-        Route::post('/update-country', [CountryController::class, 'updateCountry'])->name('update-country');
-
-        /**
-         * State Routes
-         */
-        Route::get('/state-list', [StateController::class, 'index'])->name('state_list');
-        Route::get('/add-state', [StateController::class, 'addState'])->name('add-state');
-        Route::post('/create-state', [StateController::class, 'createState'])->name('create-state');
-        Route::get('/edit-state/{id}', [StateController::class, 'editState'])->name('edit-state');
-        Route::post('/update-state', [StateController::class, 'updateState'])->name('update-state');
-        Route::get('/get-state/{id}', [StateController::class, 'getState'])->name('get-state');
-
-        /**
-         * City Routes
-         */
-        Route::get('/city-list', [CityController::class, 'index'])->name('city_list');
-        Route::get('/add-city', [CityController::class, 'addCity'])->name('add-city');
-        Route::post('/create-city', [CityController::class, 'createCity'])->name('create-city');
-        Route::get('/edit-city/{id}', [CityController::class, 'editCity'])->name('edit-city');
-        Route::post('/update-city', [CityController::class, 'updateCity'])->name('update-city');
-        Route::get('/get-city/{id}', [CityController::class, 'getCity'])->name('get-city');
         /**
          * User Routes
          */
