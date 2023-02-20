e020bb5606eb3179fa3336787b4053be319fe17d
sachin tripathi <sachin.tripathi>
2023-02-20 16:15:17 +0530

    dish variant edit

--- app/Http/Controllers/admin/DishVariantController.php
+++ app/Http/Controllers/admin/DishVariantController.php
@@ -44,43 +44,27 @@ class DishVariantController extends Controller
     }
 
     public function editDishVariant($id){
-
-        $dish = Dish::find(base64_decode($id));
-        $counter = Counter::get();
-        return view('admin-view.dish_variant.edit-variant', compact('dish','counter'));
+        $de_id = base64_decode($id);
+        $variant = DishVariant::find(base64_decode($id));
+        $dish_option = DishOption::where('dish_id',$de_id)->get();
+        return view('admin-view.dish_variant.edit-variant', compact('variant','dish_option'));
     }
 
-    public function updateDishVariant(Request $request){
+    public function updateDishVariant(Request $request, $id){
 
         $request->validate([
-            'dish_name' => 'required',
-            'dish_hsn' => 'required',
-            'counter_id' => 'required',
-            'tax_inc' => 'required',
-            'dish_has_variant' => 'required',
-
+            'price' => 'required',
         ],[
-            'dish_name.required'=>'Dish Name is Required',
-            'dish_hsn.required'=>'CIN No. is required',
-            'counter_id.required'=>'Registered Address is required',
-            'tax_inc.required'=>'Tax option is required',
-            'dish_has_variant.required'=>'state_id is required',
+            'price.required'=>'Dish Price is Required',
         ]);
-
-        $id = $request->dish_id;
-        $dish = Dish::find($id);
-        $dish->dish_name = $request->dish_name;
-        $dish->dish_price = $request->dish_price;
-        $dish->dish_code = $request->dish_code;
-        $dish->dish_hsn = $request->dish_hsn;
-        $dish->counter_id = $request->counter_id;
-        $dish->is_tax_inclusive = $request->tax_inc;
-        $dish->has_variant = $request->dish_has_variant;
         
-
-        $dish->save();
+        $id = $request->variant_id;
+        $variant = DishVariant::find($id);
+        $variant->variant_price = $request->price;
+        $variant->variant_name = implode(",", $request->options);
+        $variant->save();
         
-        return redirect()->route('edit-variant', ['id'=>base64_encode($id)])->with('success', 'Changes saved Successfully!');
+        return redirect()->route('edit-dish', ['id'=>base64_encode($id)])->with('success', 'Changes saved Successfully!');
     }
 
     public function dishvariantStatus(Request $request)
