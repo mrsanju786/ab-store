faac91d5229be085d54d59c23c7df6d756c991bc
Raxit Pansuriya <raxit.pansuriya@kanishkasoftware.com>
2023-02-27 19:05:09 +0530

    user module

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -12,40 +12,43 @@ use app\Models\User;
 
 class UserController extends Controller
 {
-    public function user() {
-        $users = User::get();
-        return view('admin-view.user', compact('users'));
+    public function index() {
+        $user = User::get();
+        return view('admin-view.user.index', compact('user'));
     }
 
     public function addUser() {
-        $permissions = Permission::get();
-        return view('admin-view.add_user', compact('permissions'));
+        $role = Role::get();
+        return view('admin-view.user.add_user', compact('role'));
     }
 
     public function createUser(Request $request) {
         $request->validate([
-            'user_name'    => 'required|unique:users,name',
+            'name'    => 'required|unique:users,name',
+            'role'    => 'required',
+            'password'    => 'required|min:6',
         ]);
-        $newUser = User::create(['name' => $request->user_name]);
-        $newUser->givePermissionTo($request->permission);
-        return redirect()->back();
+        $newUser = User::create(['name' => $request->name, 'email' => $request->email, 'password' => bcrypt($request->password)]);
+        $newUser->assignRole($request->role);
+        return redirect()->route('user-list')->with("success", "User added successfully!");
     }
 
     public function editUser($id){
         $user = User::where('id', base64_decode($id))->first();
-        $permissions = $user->permissions->pluck('name');
-        return view('admin-view.edit_user',compact('user', 'permissions'));
+        $role = Role::get();
+        return view('admin-view.user.edit_user',compact('user', 'role'));
     }
 
-    public function updateUser(Request $request, $id) {
+    public function updateUser(Request $request) {
         $request->validate([
-            'user_name'    => 'required',
+            'name'    => 'required',
+            'role'    => 'required',
         ]);
-        $edit_user = User::find(base64_decode($id));
-        $edit_user->name = $request->user_name;
+        $edit_user = User::find($request->user_id);
+        $edit_user->name = $request->name;
         $edit_user->save();
 
-        $edit_user->syncPermissions($request->permission);
-        return redirect()->back()->with("success", "User edited successfullu!");
+        $edit_user->syncRoles($request->role);
+        return redirect()->route('user-list')->with("success", "User edited successfully!");
     }
 }
---
+++ resources/views/admin-view/user/add_user.blade.php
@@ -0,0 +1,197 @@
+@extends('admin-view.layouts.main')
+@section('title')
+<title>Foodisoft | Dashboard</title>
+@endsection
+@section('content')
+
+<!--begin::Row-->
+<div class="row">
+    <!--begin::Col-->
+    <div class="col-md-12 mx-auto">
+        <div class="card card-flush h-md-100" id="kt_modal_add_role">
+            <!--begin::Card header-->
+            <div class="card-header">
+                <!--begin::Card title-->
+                <div class="card-title">
+                    <h2>Add User</h2>
+                </div>
+                <!--end::Card title-->
+            </div>
+            <!--end::Card header-->
+
+            <!--begin::Body-->
+            <div class="card-body">
+                <!--begin::Form-->
+                <form action="{{route('create-user')}}" method="POST" enctype="multipart/form-data">
+                    @csrf   
+
+                    <!--begin::Row-->
+                    <div class="row gx-9 h-100">
+                        <!--begin::Col-->
+                        <div class="col-sm-6">
+                            <!--begin::Scroll-->
+                            <div class="d-flex flex-column scroll-y me-n7 pe-7" id="kt_modal_add_role_scroll">
+
+                            </div>
+                            <!--end::Scroll-->
+                        </div>
+                        <!--end::Col-->
+                        <!--begin::Col-->
+                        <div class="col-sm-6">
+                            <!--begin::Scroll-->
+                            <div class="d-flex flex-column scroll-y me-n7 pe-p pe-md-7" id="kt_modal_add_role_scroll">
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Name</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Enter Name"
+                                        name="name">
+                                    <!--end::Input-->
+                                    @if($errors->has('name'))
+                                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Email</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input class="form-control form-control-solid" placeholder="Enter Email"
+                                        name="email">
+                                    <!--end::Input-->
+                                    @if($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->   
+                                
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Password</span>
+                                    </label>
+                                    <!--end::Label-->
+
+                                    <!--begin::Input-->
+                                    <input type="password" class="form-control form-control-solid" placeholder="Enter Password"
+                                        name="password">
+                                    <!--end::Input-->
+                                    @if($errors->has('password'))
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+                                
+                                <!--begin::Input group-->
+                                <div class="fv-row mb-10 fv-plugins-icon-container">
+                                    <!--begin::Label-->
+                                    <label class="fs-5 fw-bold form-label mb-2">
+                                        <span class="required">Select Role</span>
+                                    </label>
+                                    <!--begin::Input-->
+                                    <select class="form-select form-select-solid" aria-label="Select example" name="role" id="role" data-control="select2" >
+                                            <option value="">Select Role</option>
+                                            @foreach($role as $roles)
+                                            <option value="{{$roles->name}}">{{$roles->name}}</option>
+                                            @endforeach                                   
+                                        </select>
+                                    <!--end::Input-->
+                                    @if($errors->has('role'))
+                                    <span class="text-danger">{{ $errors->first('role') }}</span>
+                                    @endif
+                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                </div>
+                                <!--end::Input group-->
+
+                            </div>
+                            <!--end::Scroll-->
+                        </div>
+                        <!--end::Col-->
+                    </div>
+                    <!--end::Row-->
+
+                    <!--begin::Row-->
+                    <div class="row gx-9 h-100">
+                        <!--begin::Col-->
+                        <div class="col-md-12 mx-auto">
+                            <!--begin::Actions-->
+                            <div class="text-left pt-3">
+                                <button type="submit" class="btn btn-lg btn-primary"  data-kt-roles-modal-action="submit">
+                                    <span class="indicator-label">
+                                        Submit
+                                    </span>
+                                    <span class="indicator-progress">
+                                        Please wait... <span
+                                            class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                    </span>
+                                </button>
+                            </div>
+                            <!--end::Actions-->
+                        </div>
+                        <!--end::Col-->
+                    </div>
+                    <!--end::Row-->
+                </form>
+                <!--end::Form-->
+            </div>
+            <!--end::Body-->
+        </div>
+    </div>
+    <!--end::Col-->
+</div>
+<!--end::Row-->
+
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#kt_modal_add_role_form #kt_roles_select_all').on('click', function() {
+
+        if ($(this).is(':checked')) {
+            $.each($('#kt_modal_add_role_form [name="permission[]"]'), function() {
+                $(this).prop('checked', true);
+            });
+        } else {
+            $.each($('#kt_modal_add_role_form [name="permission[]"]'), function() {
+                $(this).prop('checked', false);
+            });
+        }
+
+    });
+});
+
+$('#state_id').on('change', function(){
+    var country_id = $(this).val();
+    $.ajax({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        url: "{{ url('admin/master/get-city') }}/" + country_id,
+        type: "GET",
+        success: function(response) {
+            $.each(response,function(key, value)
+            {
+                $("#city_id").append('<option value=' + value.id + '>' + value.city_name + '</option>');
+            });
+        }
+    });
+});
+</script>
+@endsection
